01a3ef3fd75217087647c2d8b52dbe60f494f7c5
Viljami Mäkelä <57489963+nucpri@users.noreply.github.com>
2022-12-12 20:35:12 +0200

    Major refactoring

--- CLog.hpp
+++ CLog.hpp
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #include <fstream>
 #include <string>
@@ -9,194 +9,154 @@
 
 using namespace std;
 
+#if __cplusplus >= 201703L
+#pragma message "test"
+#endif // __cplusplus >= 201703L
+
 // TODO: Might not be the most optimal name, didn't think too long about it
 struct ChannelConfig {
-    vector<int> channels;
-    vector<int> flags;
+	uint32_t channels;
+	uint32_t flags;
 };
 
 class CLog {
+private:
+	inline static source_location Location;
+
 public:
-    enum Channels
-    {
-        COUT,
-        CERR,
-        FILE
-    };
+	enum Channels
+	{
+		COUT = 1,
+		CERR = 2,
+		FILE = 4,
+	};
 
-    enum Flags
-    {
-        // Channel-specific flags for 'CERR'
-        // ...
-        // Channel-specific flags for 'FILE'
-        // Note to self: DO NOT ALTER ORDER, new flags should be added last
-        FORCEOVERWRITE,
-    };
+	enum Flags
+	{
+		// Channel-specific flags for 'CERR'
+		// ...
+		// Channel-specific flags for 'FILE'
+		FORCEOVERWRITE = 1,
+		//someflag = 2
+		//someflag = 4
+		// ... etc.
+	};
 
-    inline static const ChannelConfig Log = { {COUT, FILE}, {} };
-    inline static const ChannelConfig Error = { {CERR, FILE}, {} };
+	enum class MessageFormats
+	{
+		Error = 1,
+	};
 
-    /* Default Filename Override
-    **/
-    inline static string DefaultFilename = "log.txt";
+	// TODO: Make ChannelConfigs scoped
+	inline static const ChannelConfig Log = { (COUT | FILE), 0 };
+	inline static const ChannelConfig LogOW = { (COUT | FILE), FORCEOVERWRITE };
+	inline static const ChannelConfig Error = { (CERR | FILE), 0 };
 
-    /// <summary>
-    /// <para>Log to channel(s) by using a 'ChannelConfig'</para>
-    /// <para>‎ </para>
-    /// <para>QUICK USAGE:</para>
-    /// <para>Log::ToChannels("Some message");</para>
-    /// <para>‎</para>
-    /// <para>Log an Error:</para>
-    /// <para>Log::ToChannels("Some message", Log::Error);</para>
-    /// </summary>
-    static void ToChannels(string Message, ChannelConfig cc, const source_location Location = source_location::current()) {
-        for (int channel : cc.channels) {
-            auto now = chrono::system_clock::now();
-            string formatted_time = std::format("{0:%F_%T}", now);
+	inline static string DefaultFilename = "log.txt";
 
-            // Available flags
-            bool forceOverwrite = false;
+	/// <summary>
+	/// <para>Log to channel(s) by using a 'ChannelConfig'</para>
+	/// <para>‎ </para>
+	/// <para>QUICK USAGE:</para>
+	/// <para>CLog::ToChannels("Some message");</para>
+	/// <para>‎</para>
+	/// <para>Log an Error:</para>
+	/// <para>CLog::ToChannels("Some message", CLog::Error);</para>
+	/// </summary>
+	static void ToChannels(string Message, ChannelConfig cc = Log, const source_location Location = source_location::current()) {
+		CLog::Location = Location; // Provide location for helper functions, such as Format()
+		string filename = DefaultFilename;
 
-            // Process flags
-            for (int flag : cc.flags) {
-                if (flag == FORCEOVERWRITE) forceOverwrite = true;
-            }
+		if (cc.channels & COUT) cout << Message;
 
-            // TODO: Find a way to provide access for filename .. a 'packet' parameter wouldn't be nice, maybe.. would it?
-            // Maybe not.
-            string filename = DefaultFilename;
+		// Logs to std::cerr, see: https://cplusplus.com/reference/iostream/cerr/
+		if (cc.channels & CERR) cerr << Format(Message, (int)MessageFormats::Error);
 
-            switch (channel)
-            {
-            case COUT:
-                cout << formatted_time <<
-                    "\nFile: " <<
-                    Location.file_name() << "(" <<
-                    Location.line() << ":" <<
-                    Location.column() << ") \n" <<
-                    Location.function_name() << ": " <<
-                    Message << endl << endl;
-                break;
-            case CERR:
-                cerr << formatted_time <<
-                    "\nFile: " <<
-                    Location.file_name() << "(" <<
-                    Location.line() << ":" <<
-                    Location.column() << ") \n" <<
-                    Location.function_name() << ": " <<
-                    Message << endl << endl;
-                break;
-            case FILE:
-                // Check if a file exists
-                if (auto readFile = ifstream(filename) && !forceOverwrite)
-                {
-                    // File exists?: Append
-                    ofstream outfile(filename, std::ios_base::app);
-                    outfile << formatted_time <<
-                        "\nFile: " <<
-                        Location.file_name() << "(" <<
-                        Location.line() << ":" <<
-                        Location.column() << ") \n" <<
-                        Location.function_name() << ": " <<
-                        Message << endl << endl;
-                    outfile.close();
-                }
-                else
-                {
-                    // File doesn't exist?: Overwrite
-                    ofstream outfile(filename);
-                    outfile << formatted_time <<
-                        "\nFile: " <<
-                        Location.file_name() << "(" <<
-                        Location.line() << ":" <<
-                        Location.column() << ") \n" <<
-                        Location.function_name() << ": " <<
-                        Message << endl << endl;
-                    outfile.close();
-                }
-                break;
-            default:
-                break;
-            }
-        }
-    }
+		// Logs into a file (no formatting), location specified in 'DefaultFilename'
+		if (cc.channels & FILE) {
+			if (auto readFile = ifstream(filename))
+			{
+				ofstream outfile;
+				if (cc.flags & FORCEOVERWRITE) outfile.open(filename);
+				else outfile.open(filename, std::ios_base::app);
+				outfile << Message;
+				outfile.close();
+			}
+		}
 
-    /// <summary>
-    /// <para>Log to channel(s)</para>
-    /// <para>‎ </para>
-    /// <para>EXAMPLES</para>
-    /// <para>Write to std::cerr and to a file</para>
-    /// <para>Log::ToChannels("someMessage", { CERR, FILE });</para>
-    /// <para>‎ </para>
-    /// <para>Write to a custom file:</para>
-    /// <para>Log::DefaultFilename = "myLogFile.txt";</para>
-    /// <para>ToChannels("someMessage", { FILE });</para>
-    /// </summary>
-    /// 
-    /// <param name="Message"></param>
-    /// <param name="Channels"></param>
-    /// <param name="Flags"></param>
-    /// <param name="Location"></param>
-    static void ToChannels(string Message, vector<int> Channels = {CERR, FILE}, vector<int> Flags = {}, const source_location Location = source_location::current()) {
-        for (int channel : Channels) {
-            auto now = chrono::system_clock::now();
-            string formatted_time = std::format("{0:%F_%T}", now);
-             
+		// Logs into a file (error formatting, location specified in 'DefaultFilename'
+		if (cc.channels & FILE | CERR) {
+			if (auto readFile = ifstream(filename))
+			{
+				ofstream outfile;
+				if (cc.flags & FORCEOVERWRITE) outfile.open(filename);
+				else outfile.open(filename, std::ios_base::app);
+				outfile << Format(Message, (int)MessageFormats::Error);
+				outfile.close();
+			}
+		}
+	}
 
-            // Available flags
-            bool forceOverwrite = false;
+	/// <summary>
+	/// <para>Log to channel(s)</para>
+	/// <para>‎ </para>
+	/// <para>EXAMPLES</para>
+	/// <para>Write to std::cerr and to a file:</para>
+	/// <para>CLog::ToChannels("someMessage", CLog::CERR | CLog::FILE });</para>
+	/// <para>‎ </para>
+	/// <para>Write to a custom file:</para>
+	/// <para>CLog::DefaultFilename = "myLogFile.txt";</para>
+	/// <para>CLog::ToChannels("someMessage", CLog::FILE );</para>
+	/// </summary>
+	/// 
+	/// <param name="Message"></param>
+	/// <param name="Channels"></param>
+	/// <param name="Flags"></param>
+	/// <param name="Location"></param>
+	static void ToChannels(string Message, uint32_t Channels, uint32_t Flags = {}, const source_location Location = source_location::current()) {
+		string formatted_time = std::format("{0:%F_%T}", chrono::system_clock::now());
 
-            // Process flags
-            for (int flag : Flags) {
-                if (flag == FORCEOVERWRITE) forceOverwrite = true;
-            }
+		// TODO: Find a way to provide access for filename .. a 'packet' parameter wouldn't be nice, maybe.. would it?
+		string filename = DefaultFilename;
 
-            // TODO: Find a way to provide access for filename .. a 'packet' parameter wouldn't be nice, maybe.. would it?
-            string filename = DefaultFilename;
+		const string error_message = formatted_time + "\nFile: " +
+			Location.file_name() + "(" +
+			to_string(Location.line()) + ":" +
+			to_string(Location.column()) + ") \n" +
+			Location.function_name() + ": " +
+			Message + "\n\n";
 
-            switch (channel)
-            {
-            case CERR:
-                cerr << formatted_time <<
-                "\nFile: " <<
-                Location.file_name() << "(" <<
-                Location.line() << ":" <<
-                Location.column() << ") \n" <<
-                Location.function_name() << ": " <<
-                Message << endl << endl;
-                break;
-            case FILE:
-                // Check if a file exists
-                if (auto readFile = ifstream(filename) && !forceOverwrite)
-                {
-                    // File exists?: Append
-                    ofstream outfile(filename, std::ios_base::app);
-                    outfile << formatted_time <<
-                        "\nFile: " <<
-                        Location.file_name() << "(" <<
-                        Location.line() << ":" <<
-                        Location.column() << ") \n" <<
-                        Location.function_name() << ": " <<
-                        Message << endl << endl;
-                    outfile.close();
-                }
-                else
-                {
-                    // File doesn't exist?: Overwrite
-                    ofstream outfile(filename);
-                    outfile << formatted_time <<
-                        "\nFile: " <<
-                        Location.file_name() << "(" <<
-                        Location.line() << ":" <<
-                        Location.column() << ") \n" <<
-                        Location.function_name() << ": " <<
-                        Message << endl << endl;
-                    outfile.close();
-                }
-                break;
-            default:
-                break;
-            }
-        }
-    }
+		// Logs to std::cerr, see: https://cplusplus.com/reference/iostream/cerr/
+		if (Channels & CERR) {
+			cerr << error_message;
+		}
+
+		// Logs into a file, location specified in 'DefaultFilename'
+		if (Channels & FILE) {
+			if (auto readFile = ifstream(filename))
+			{
+				ofstream outfile;
+				if (Flags & FORCEOVERWRITE) outfile.open(filename);
+				else outfile.open(filename, std::ios_base::app);
+				outfile << error_message;
+				outfile.close();
+			}
+		}
+	}
+
+	// Formats a 'Message' (defaults to ERROR format)
+	// TODO: add useful comments
+	static string Format(string Message, uint32_t Format = (int)MessageFormats::Error) {
+		//TODO: Some method to introduce custom format easily without touching the class code
+		string formatted_time = std::format("{0:%F_%T}", chrono::system_clock::now());
+
+		if (Format & (int)MessageFormats::Error) {
+			return formatted_time + "\nFile: " +
+				Location.file_name() + "(" +
+				to_string(Location.line()) + ":" +
+				to_string(Location.column()) + ") \n" +
+				Location.function_name() + ": " +
+				Message + "\n\n";
+		}
+	}
 };
